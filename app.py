8d1f2de579e5150a222750d48b08c189e3e500e8
maschler <fabian.maschler@student.hpi.uni-potsdam.de>
2016-06-01 15:00:02 +0200

    Comment async modes due to errors

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ from query_hyrise import benchmark, query_hyrise
 async_mode = None
 
 if async_mode is None:
-    try:
-        import eventlet
-        async_mode = 'eventlet'
-    except ImportError:
-        pass
+    #try:
+    #    import eventlet
+    #    async_mode = 'eventlet'
+    #except ImportError:
+    #    pass
 
     if async_mode is None:
         try:
@@ -35,12 +35,12 @@ if async_mode is None:
     print('async_mode is ' + async_mode)
 
 # monkey patching is necessary because this application uses a background thread
-if async_mode == 'eventlet':
-    import eventlet
-    eventlet.monkey_patch()
-elif async_mode == 'gevent':
-    from gevent import monkey
-    monkey.patch_all()
+#if async_mode == 'eventlet':
+#    import eventlet
+#    eventlet.monkey_patch()
+#elif async_mode == 'gevent':
+#    from gevent import monkey
+#    monkey.patch_all()
 
 
 
