5d54bca36ca6a85513ff6c6c7dfc1e21a073ee8a
Masterprojekt Sommersemester 2016 <fabian.maschler@student.hpi.uni-potsdam.de>
2016-07-06 14:23:54 +0200

    Use Upstart to run server

--- provision.sh
+++ provision.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y git python3-pip python3-dev npm
 cp -r /vagrant/ ~/hyrise_rcm
 cd hyrise_rcm/
 sudo pip3 install -r requirements.txt 
+sudo cp hyrise_rcm.conf /etc/init/
  
 cd static
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
@@ -22,4 +23,3 @@ sudo apt-get install -y python3.5
 
 sudo rm -r /usr/local/lib/python3.5/dist-packages/
 sudo ln -s /usr/local/lib/python3.4/dist-packages /usr/local/lib/python3.5/dist-packages
-
