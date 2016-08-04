b000933e0ac4ff435d1138ccafc028bb2245bf31
maschler <fabian.maschler@student.hpi.uni-potsdam.de>
2016-08-04 12:32:05 +0200

    Switch to python2.7

--- provision.sh
+++ provision.sh
@@ -3,11 +3,11 @@ set -ex
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y git python3-pip python3-dev npm
+sudo apt-get install -y python-pip python-dev npm
 
 cp -r /vagrant/ ~/hyrise_rcm
 cd hyrise_rcm/
-sudo pip3 install -r requirements.txt 
+sudo pip install -r requirements.txt 
 sudo cp hyrise_rcm.conf /etc/init/
  
 cd static
@@ -15,14 +15,6 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm install | xargs echo
 npm run tsc || true
-
-# Install python3.5 and link python3.4 packages
-sudo add-apt-repository ppa:fkrull/deadsnakes -y
-sudo apt-get update
-sudo apt-get install -y python3.5
-
-sudo rm -r /usr/local/lib/python3.5/dist-packages/
-sudo ln -s /usr/local/lib/python3.4/dist-packages /usr/local/lib/python3.5/dist-packages
 
 sudo service hyrise_rcm start
 
